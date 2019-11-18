3fdde8bcc4c1f99215cdcc5b7d2ea221dcb792c9
R5147 <flightd0321210@hotmail.com>
2019-11-18 22:33:19 +0800

    Create days_old_suggested_answer.py

---
+++ CS101/lesson_8/days_old_suggested_answer.py
@@ -0,0 +1,61 @@
+def isLeapYear(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+    # assert dateIsBefore(year1, month1, day1, year2, month2, day2)
+    # YOUR CODE HERE!
+    day = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        day += 1
+    return day
+
+print(daysBetweenDates(2012,1,1,2012,2,28))
+print(daysBetweenDates(2012,1,1,2012,3,1))
+print(daysBetweenDates(2011,6,30,2012,6,30))
+print(daysBetweenDates(2011,1,1,2012,8,8))
+print(daysBetweenDates(1900,1,1,1999,12,31))
