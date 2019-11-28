9ad24cdd1686e003e07ea712310079b19a9c7ae2
R5147 <flightd0321210@hotmail.com>
2019-11-28 13:02:58 +0800

    Create open_source_file.py

---
+++ open_source_file.py
@@ -0,0 +1,52 @@
+import os
+import pyperclip
+def open_source_file(content):
+    # open source code [start] reference: https://datatofish.com/command-prompt-python/
+
+    lang_sequence = [0, 0, 0, 0, 0]
+
+    lang_type = [
+        ['en', 'tc', 'sc'],
+        ['english', 'tc_chi', 'sc_chi']
+    ]
+
+    for iindex, i in enumerate(lang_type):
+        for jindex, j in enumerate(i):
+            k = '/' + j + '/'
+            if content.find(k) != -1:
+                lang_sequence[0] = jindex
+                lang_sequence[2] = jindex
+                if jindex == 0:
+                    lang_sequence[1] = 1
+                    lang_sequence[3] = 2
+                elif jindex == 1:
+                    lang_sequence[1] = 2
+                    lang_sequence[3] = 0
+                elif jindex == 2:
+                    lang_sequence[1] = 1
+                    lang_sequence[3] = 0
+                lang_sequence[4] = iindex
+                break
+
+    url_head = {
+        'https://uat1.netsoft.net': '\\\\192.168.1.4\\common_website',
+        'https://srpa.netsoft.net': '\\\\192.168.1.4\\common_website\\srpa'
+    }
+
+    for head in url_head:
+        if content.startswith(head):
+            string = content
+            string = string.replace(head, url_head[head])
+            string = string.replace('/', '\\')
+
+            for i in string.split('\n'):
+                print(i)
+                os.system('cmd /c "{} & {} & {}"'.format(i, i.replace(
+                    lang_type[lang_sequence[4]][lang_sequence[0]],
+                    lang_type[lang_sequence[4]][lang_sequence[1]]), i.replace(
+                    lang_type[lang_sequence[4]][lang_sequence[2]],
+                    lang_type[lang_sequence[4]][lang_sequence[3]])))
+            break
+    # open source code [end]
+
+open_source_file(pyperclip.paste())
